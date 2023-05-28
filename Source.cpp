223c904324ee1f08bc2f687ea8d9aebf678dd9a5
cppandria <vadim.priy@gmail.com>
2023-05-28 21:31:07 +0300

    first commit

--- Source.cpp
+++ Source.cpp
@@ -1,50 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <sstream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-//additional recursive function
-int countChars(string s, char c) {
-	if (s.length() == 0)
-		return 0;
-	else if (s.at(0) == c)
-		return 1 + countChars(s.substr(1), c);
-	else
-		return countChars(s.substr(1), c);
-}
-//main recursive funstion
-int get_count_from_str(string file_content, string key) {
+int get_count_from_file(char* file_content, char* key) {
+
 	int count = 0;
-	while (key.length() != 0) {
-		count += countChars(file_content, key.at(0));
-		key = key.substr(1);
+	int content_lenght = strlen(file_content);
+	int key_lenght = strlen(key);
+	for (int i = 0; i < key_lenght; i++) {
+		for (int j = 0; j < content_lenght; j++) {
+			if (file_content[j] == key[i])
+			{
+				count++;
+			}
+		}
 	}
 	return count;
 }
 
+char *str_replace(char *orig, char *rep, char *with) {
+	char *result;
+	char *ins;
+	char *tmp;
+	int len_rep;
+	int len_with;
+	int len_front;
+	int count;
+	char empty[] = "";
 
-string replace_string(string inputStr, string src, string dst)
-{
-	std::string result(inputStr);
-	
-	size_t pos = result.find(src);
-	while (pos != std::string::npos) {
-		result.replace(pos, src.size(), dst);
-		pos = result.find(src, pos);
+	if (!orig || !rep)
+		return NULL;
+	len_rep = strlen(rep);
+	if (len_rep == 0)
+		return NULL;
+	if (!with)
+		with = empty;
+	len_with = strlen(with);
+
+	ins = orig;
+	for (count = 0; tmp = strstr(ins, rep); ++count) {
+		ins = tmp + len_rep;
 	}
 
+	tmp = result = (char*)malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+	if (!result)
+		return NULL;
+
+	while (count--) {
+		ins = strstr(orig, rep);
+		len_front = ins - orig;
+		tmp = strncpy(tmp, orig, len_front) + len_front;
+		tmp = strcpy(tmp, with) + len_with;
+		orig += len_front + len_rep;
+	}
+	strcpy(tmp, orig);
 	return result;
 }
-
 int main() {
 
-	string key_word = "BASIC";
-	string example_string = "This is text about basic language. Basic is the best! Remember that only case sensitive LETTERS could be represented as substrings of the word BASIC. So word basic will have count of 0 matched letters and BASIC will have 5 count!    Best regards,    SIGN ME.txt";
+	char key_word[] = "BASIC";
+	char example_string[] = "This is text about basic language. Basic is the best! Remember that only case sensitive LETTERS could be represented as substrings of the word BASIC. So word basic will have count of 0 matched letters and BASIC will have 5 count!		Best regards,		SIGN ME.txt";
 
-	cout << "Line is:\n" << example_string << endl << endl;
-	cout << "Count =" << get_count_from_str(example_string, key_word) << endl;
+	printf("Line is %s \n", example_string);
 
-	cout << "Line is:\n" << replace_string(example_string, key_word, "Delphi") << endl << endl;
+	printf("Count =%d \n", get_count_from_file(example_string, key_word));
+	char replacer[] = "Delphi";
+	printf("Line is: %s \n", str_replace(example_string, key_word, replacer));
 
 	return 0;
 }
